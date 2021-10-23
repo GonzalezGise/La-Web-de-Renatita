66088ac5cbb7f36096a11983a89a8c83ecd57ae5
lucho1997 <lucsan1997@gmail.com>
2021-10-23 01:12:36 -0300

    avance index

---
+++ js/app.js
@@ -0,0 +1,14 @@
+jQuery('document').ready(function($){
+
+    var icono_menu = $('.icono-menu'),
+        menu = $('nav ul');
+
+
+
+icono_menu.click(function(){
+    menu.addClass('.show');
+
+});
+
+
+});
